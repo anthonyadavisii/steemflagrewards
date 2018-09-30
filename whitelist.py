c4c1fdeae0315c8faf347f8e1ce61b92be65efcf
Anthony A Davis <35675472+anthonyadavisii@users.noreply.github.com>
2018-09-30 09:46:22 -0500

    Whitelist for Flag Mention Automatic Approvals

---
+++ whitelist.py
@@ -0,0 +1,21 @@
+WL = [
+              'anthonyadavisii',
+              'crokkon',
+              'enforcer48',
+              'flugschwein',
+              'hanen',
+              'steemseph',
+              'ipromote',
+              'themarkymark',
+              'thedarkhorse',
+              'slobberchops',
+              'iamstan',
+              'lovenfreedom',
+              'mathowl'
+              'jplaughing',
+              'kabir88',
+              'kingsofa',
+              'pjau',
+              'serylt',
+              'steevc',
+              ]
